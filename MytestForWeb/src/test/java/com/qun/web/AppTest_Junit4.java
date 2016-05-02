3d1fdad0c85bd26402045dbcd8e8018a0e0180e3
佳俊 <@jun-pc>
2016-05-03 00:10:05 +0800

    junit4

--- MytestForWeb/src/test/java/com/qun/web/AppTest_Junit4.java
+++ MytestForWeb/src/test/java/com/qun/web/AppTest_Junit4.java
@@ -1,37 +1,38 @@
 package com.qun.web;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
- * junit3实现方式，junit4后实现注解
+ * copy AppTest实现junit4的测试类
  */
-public class AppTest extends TestCase {
+public class AppTest_Junit4{
 	/**
 	 * Create the test case
 	 *
 	 * @param testName name of the test case
 	 */
-	public AppTest(String testName) {
+	/*public AppTest_Junit4(String testName) {
 		super(testName);
-	}
+	}*/
 
 	/**
 	 * @return the suite of tests being tested
 	 */
-	public static Test suite() {
-		return new TestSuite(AppTest.class);
-	}
+	/*public static Test suite() {
+		return new TestSuite(AppTest_Junit4.class);
+	}*/
 
 	/**
 	 * Rigourous Test :-)
 	 */
+	@Test
 	public void testApp() {
 		assertTrue(true);
 	}
 	
+	@Test
 	public void testAdd(){
 		assertEquals(5, new App().add(2, 3));
 	}
